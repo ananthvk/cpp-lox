21b33a2480ff284c5da2218082e06ad7dd2f34f9
ananthvk <mailstoanantha@gmail.com>
2025-06-27 21:31:30 +0530

    Fix issues with build system

--- development.sh
+++ development.sh
@@ -3,6 +3,6 @@
 mkdir -p subprojects
 meson wrap install fmt
 meson wrap install google-benchmark
-CXX=g++ CC=gcc meson setup -Db_sanitize=address -Ddevelopment=true -Db_lundef=false -Denable-tests=true -Denable-benchmarks=true --reconfigure builddir
+CXX=clang++ CC=clang meson setup -Db_sanitize=address -Ddevelopment=true -Db_lundef=false -Denable-tests=true -Denable-benchmarks=true --reconfigure builddir
 cd builddir
 meson test -v
