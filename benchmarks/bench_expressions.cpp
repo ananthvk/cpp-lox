71afd70ec1046b89b8b70da8e4878f931c31b8ac
ananthvk <mailstoanantha@gmail.com>
2025-06-27 21:54:32 +0530

    Add benchmark to benchmark global variable

--- benchmarks/bench_expressions.cpp
+++ benchmarks/bench_expressions.cpp
@@ -44,7 +44,8 @@ static void BM_CompileExpression(benchmark::State &state)
     ErrorReporter reporter;
     Allocator allocator;
     const char *src =
-        "print (44*63-(-61*-47--49/(51/(93*-21-(((-32*(11*-50-(((52*-39-(48/(-33/(-96*77-76/99-(((91/"
+        "print "
+        "(44*63-(-61*-47--49/(51/(93*-21-(((-32*(11*-50-(((52*-39-(48/(-33/(-96*77-76/99-(((91/"
         "-75-60/-3-87*83--75/19)*79-59*43-85/-50--25*-92)/90-58/67--26*35-33*-100)/69-95/-29)-39/"
         "-17--57*65--3*-90)-42*75--11*-70-92*55)*-78+-41/85-34/-57)*62-11/-87-29/93-34/2)/-15--23/"
         "-41-61/-75--92*-74)/-83--82/93--59/68)-21*82--7*7-16*-69)/-13-82*-8+58*73-29*43)/"
@@ -94,5 +95,82 @@ static void BM_RunExpressionNegation(benchmark::State &state)
 }
 
 BENCHMARK(BM_RunExpressionNegation);
+
+static void BM_GlobalVariables(benchmark::State &state)
+{
+    ErrorReporter reporter;
+    Allocator allocator;
+    VMOpts opts;
+    VM vm(opts, reporter, allocator);
+    const char *src =
+        "var a = 3;\n"
+        "var b = 77;\n"
+        "var c = 180;\n"
+        "var d = 15;\n"
+        "var abcdefghij = 20;\n"
+        "var x;\n"
+        "var l;\n"
+        "var abc12345;\n"
+        "var ab;\n"
+        "var bc;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "abcdefghij = abcdefghij + 1;\n"
+        "print abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print "
+        "abcdefghij;print "
+        "abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print "
+        "abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print "
+        "abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print "
+        "abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print "
+        "abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print "
+        "abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print "
+        "abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print "
+        "abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print "
+        "abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print "
+        "abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print "
+        "abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print "
+        "abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print "
+        "abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print "
+        "abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print "
+        "abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print "
+        "abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print "
+        "abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print "
+        "abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print "
+        "abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;print abcdefghij;";
+    auto chunk = compile(src, reporter, allocator);
+    for (auto _ : state)
+        execute(chunk, vm);
+}
+
+BENCHMARK(BM_GlobalVariables);
 
 BENCHMARK_MAIN();
