8ff8307abda0fd9b3901618652849fb8d86fce6b
ananthvk <mailstoanantha@gmail.com>
2025-10-10 02:58:57 +0530

    add tests

---
+++ tests/test_functions_recursion.py
@@ -0,0 +1,84 @@
+def test_factorial_recursion(run_lox):
+    assert (
+        run_lox(
+            """
+    fun factorial(n) {
+        if (n <= 1) return 1;
+        return n * factorial(n - 1);
+    }
+    print factorial(5);
+    """
+        )
+        == "120"
+    )
+
+
+def test_fibonacci_recursion(run_lox):
+    assert (
+        run_lox(
+            """
+    fun fib(n) {
+        if (n <= 1) return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+    print fib(7);
+    """
+        )
+        == "13"
+    )
+
+
+def test_countdown_recursion(run_lox):
+    assert (
+        run_lox(
+            """
+    fun countdown(n) {
+        if (n <= 0) {
+            print "Done!";
+            return;
+        }
+        print n;
+        countdown(n - 1);
+    }
+    countdown(3);
+    """
+        )
+        == "3\n2\n1\nDone!"
+    )
+
+
+def test_sum_recursion(run_lox):
+    assert (
+        run_lox(
+            """
+    fun sum(n) {
+        if (n <= 0) return 0;
+        return n + sum(n - 1);
+    }
+    print sum(4);
+    """
+        )
+        == "10"
+    )
+
+
+def test_mutual_recursion(run_lox):
+    assert (
+        run_lox(
+            """
+    fun isEven(n) {
+        if (n == 0) return true;
+        return isOdd(n - 1);
+    }
+    
+    fun isOdd(n) {
+        if (n == 0) return false;
+        return isEven(n - 1);
+    }
+    
+    print isEven(4);
+    print isOdd(3);
+    """
+        )
+        == "true\ntrue"
+    )
