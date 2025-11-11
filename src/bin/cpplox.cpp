2aac65d70e869b0db5283cf710405a0e84c207d3
ananthvk <mailstoanantha@gmail.com>
2025-11-12 00:04:23 +0530

    ci: fix gh actions release for windows

--- src/bin/cpplox.cpp
+++ src/bin/cpplox.cpp
@@ -75,7 +75,7 @@ int main(int argc, char *argv[])
 
         if (result.count("version"))
         {
-            fmt::println("v1.0.5");
+            fmt::println("v1.0.6");
             exit(0);
         }
 
