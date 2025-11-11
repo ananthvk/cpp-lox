68e10f2a5f0c33c118e311fa9c36b275cd9685a4
ananthvk <mailstoanantha@gmail.com>
2025-11-12 00:09:03 +0530

    ci: fix gh actions release

--- src/bin/cpplox.cpp
+++ src/bin/cpplox.cpp
@@ -75,7 +75,7 @@ int main(int argc, char *argv[])
 
         if (result.count("version"))
         {
-            fmt::println("v1.0.6");
+            fmt::println("v1.0.7");
             exit(0);
         }
 
